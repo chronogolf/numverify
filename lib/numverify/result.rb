668f64a2ce6244e59fb53c799063d512606c5658
Olivier Buffon <olivier@chronogolf.ca>
2017-06-27 18:30:56 -0400

    Create result object

---
+++ lib/numverify/result.rb
@@ -0,0 +1,27 @@
+module NumverifyClient
+  class Result
+    def initialize(data = {})
+      convert_to_obj(data)
+    end
+
+    def attributes
+      instance_variables.each_with_object({}) do |instance_variable, h|
+        h[instance_variable[1..instance_variable.length]] = instance_variable_get(instance_variable)
+      end
+    end
+
+    def to_json
+      attributes.to_json
+    end
+
+    private
+
+    def convert_to_obj(h)
+      h.each do |key, value|
+        self.class.send(:attr_accessor, key)
+        instance_variable_set("@#{key}", value)
+        convert_to_obj(value) if value.is_a? Hash
+      end
+    end
+  end
+end
