15afb2e212ca117b55c8a26a6c117ed837cfadb3
Olivier Buffon <olivier@chronogolf.ca>
2017-06-27 18:31:26 -0400

    Create validation request logic

--- lib/numverify.rb
+++ lib/numverify.rb
@@ -1,5 +1,26 @@
-require "numverify/version"
+require 'excon'
+require 'json'
+require 'numverify/version'
+require 'numverify/request'
 
-module Numverify
-  # Your code goes here...
+module NumverifyClient
+  class << self
+    def validate(number:, country_code: nil, access_key: ENV['NUMVERIFY_ACCESS_KEY'])
+      request(build_query(number, country_code, access_key)).perform(method: :get)
+    end
+
+    private
+
+    def build_query(number, country_code, access_key)
+      {
+        number: number,
+        country_code: country_code,
+        access_key: access_key
+      }
+    end
+
+    def request(query)
+      NumverifyClient::Request.new(query)
+    end
+  end
 end
